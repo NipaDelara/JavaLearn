964624ee95fcaeb488b523ff0910c03dd15fe526
NipaDelara <nipadelara@hotmail.com>
2026-03-17 22:40:21 +0200

    Task 2 completed, the code runs successfully

---
+++ src/Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+    static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
