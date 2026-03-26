4461d2486ea09ec8ae18f104139ee871a247eb36
NipaDelara <nipadelara@hotmail.com>
2026-03-26 22:29:03 +0200

    assignment 2.1 task 1 done

---
+++ assignment2.1/src/Television.java
@@ -0,0 +1,31 @@
+public class Television {
+    private int channel;
+    private boolean on;
+
+    public Television() {
+        this.channel = 1;
+        this.on = false;
+    }
+
+    public void setChannel(int channel) {
+        if (channel > 10) {
+            this.channel = 1;
+        } else if (channel < 1) {
+            this.channel = 1;
+        } else {
+            this.channel = channel;
+        }
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void pressOnOff() {
+        on = !on;
+    }
+}
