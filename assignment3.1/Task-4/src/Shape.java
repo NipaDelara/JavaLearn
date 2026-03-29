d702781347715ce28aca5b17f59a868773441f08
NipaDelara <nipadelara@hotmail.com>
2026-03-29 21:44:35 +0300

    assignment 3.1 Task 4: Shape properties done

---
+++ assignment3.1/Task-4/src/Shape.java
@@ -0,0 +1,16 @@
+public class Shape {
+
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public double calculateArea() {
+        return 0;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
